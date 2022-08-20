688b191bc743852a52ce062aa2ac817cb424a059
Ahmed Ashraf <ahmdgun0@gmail.com>
2022-08-20 07:38:56 +0200

    Update explore.js

--- Routes/posts/get/explore.js
+++ Routes/posts/get/explore.js
@@ -31,7 +31,7 @@ const getExplore = async (req, res) => {
   let total = await Posts.count();
   total = Math.ceil(total / amount);
 
-  if (!posts.at(-1)?.caption) {
+  if (!posts.at(-1)?.img) {
     return res.status(200).send({ total, posts: [] });
   }
 
